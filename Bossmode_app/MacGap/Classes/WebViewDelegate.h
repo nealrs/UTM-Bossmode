39a46141772149b3e717ca4ecc19a51099940d14
Neal Shyam <neal@audioshocker.com>
2014-02-15 15:32:04 -0500

    bossmode app

---
+++ Bossmode_app/MacGap/Classes/WebViewDelegate.h
@@ -0,0 +1,39 @@
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@class Sound;
+@class Dock;
+@class Growl;
+@class Notice;
+@class Path;
+@class App;
+@class Window;
+@class Clipboard;
+
+@class WindowController;
+
+@interface WebViewDelegate : NSObject {
+	Sound* sound;
+    Dock* dock;
+    Growl* growl;
+    Notice* notice;
+    Path* path;
+    App* app;
+    Window* window;
+    Clipboard* clipboard;
+}
+
+
+
+@property (nonatomic, retain) Sound* sound;
+@property (nonatomic, retain) Dock* dock;
+@property (nonatomic, retain) Growl* growl;
+@property (nonatomic, retain) Notice* notice;
+@property (nonatomic, retain) Path* path;
+@property (nonatomic, retain) App* app;
+@property (nonatomic, retain) Window* window;
+@property (nonatomic, retain) Clipboard* clipboard;
+
+@property (nonatomic, retain) WindowController *requestedWindow;
+
+@end
